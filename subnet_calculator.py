9798fe1c6870d28dd8aa9d6b9b6108e90b814a64
johnnydrama92 <james.ferrier01@gmail.com>
2018-01-26 21:17:36 +0000

    updated for user input

--- subnet_calculator.py
+++ subnet_calculator.py
@@ -8,7 +8,15 @@ input_mask =[]
 for oct in mask:
     input_mask.append(int(oct))
 
-
+"""
+network id is calculated using a BINARY AND operation on the ip address and the mask.
+each output of the output is 1 ONLY IF the corresponding bit in x and y is 1 else its 0
+example:
+192.168.0.1
+255.255.255.0
+=
+192.168.0.0
+"""
 
 def network_id(ip, mask):
     id = []
@@ -19,6 +27,17 @@ def network_id(ip, mask):
         iterator += 1
     return id
 
+
+"""
+broadcast is calculated using a BINARY OR on the network id and the wildcard
+each bit of the output is 0 ONLY IF the corresponding bit of x and y is 0, else its 1
+example:
+192.168.0.1
+0.0.0.255
+=
+192.168.0.255
+"""
+
 def broadcast(net_id, wildcard):
     broadcast_addr = []
     iterator = 0
@@ -27,6 +46,11 @@ def broadcast(net_id, wildcard):
         broadcast_addr.append(x)
         iterator += 1
     return broadcast_addr
+
+"""
+first ip address is network id + 1
+"""
+
 
 def first_ip(ip):
     first = []
@@ -44,6 +68,11 @@ def first_ip(ip):
         first.append(int(bin_oct, base=2))
     return first
 
+"""
+last ip address is broadcast - 1
+"""
+
+
 def last_ip(ip):
     last = []
     binary_list = []
@@ -60,6 +89,31 @@ def last_ip(ip):
         last.append(int(bin_oct, base=2))
     return last
 
+"""
+wildcard is is inverse of the mask, this can be calculated using a BINARY NOT operation
+example:
+11001110
+00110001
+
+bitewise NOT can be achieved like so:
+-x -1
+this switches the 1s and 0s 
+a bitewise shift is necessary here
+
+x = (1 << 8) is another way of writing x = 256
+100000000 = 256
+see the 1 is shifted 8 bits to the left
+
+x = (1 << 8) - 1 = 255
+formula:
+where var is our octet in question
+x = (1 << 8) - 1 - var = inverse
+example:
+x = (1 << 8) - 1 - 128 = 127
+127 is the 8 bit binary inverse of 128
+x = (1 << 8) - 1 - 0 = 255
+255 is the 8 bit binary inverse of 0
+"""
 
 def wildcard(mask):
     wild = []
@@ -68,6 +122,18 @@ def wildcard(mask):
         wild.append(x)
     return wild
 
+"""
+the number of ips is just the wildcard - 1
+/24 example:
+0.0.0.255 - 1 = 254
+/16 example:
+0.0.255.255
+=
+00000000000000001111111111111111
+=
+65535
+65535 - 1 = 65534
+"""
 def number_of_ips(wildcard):
     wilc_base_2 = []
     for oct in wildcard:
@@ -80,12 +146,12 @@ def number_of_ips(wildcard):
 
 
 
-
-print("NETWORK ID:      -   {}".format(network_id(input_ip, input_mask)))
-print("BROADCAST:       -   {}".format(broadcast(network_id(input_ip, input_mask), wildcard(input_mask))))
-print("FIRST ADDRESS:   -   {}".format(first_ip(network_id(input_ip, input_mask))))
-print("LAST ADDRESS:    -   {}".format(last_ip(broadcast(network_id(input_ip, input_mask), wildcard(input_mask)))))
-print("WILDCARD:        -   {}".format(wildcard(input_mask)))
-print("NUMBER OF HOSTS: -   {}".format(number_of_ips(wildcard(input_mask))))
-
-input()
+try:
+    print("NETWORK ID:      -   {}".format(network_id(input_ip, input_mask)))
+    print("BROADCAST:       -   {}".format(broadcast(network_id(input_ip, input_mask), wildcard(input_mask))))
+    print("FIRST ADDRESS:   -   {}".format(first_ip(network_id(input_ip, input_mask))))
+    print("LAST ADDRESS:    -   {}".format(last_ip(broadcast(network_id(input_ip, input_mask), wildcard(input_mask)))))
+    print("WILDCARD:        -   {}".format(wildcard(input_mask)))
+    print("NUMBER OF HOSTS: -   {}".format(number_of_ips(wildcard(input_mask))))
+except:
+    print("Error")
